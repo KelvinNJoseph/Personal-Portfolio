1e189c4dd96e6eadd38addb0771af4daa4da9ab9
Kelvin Joseph <kelvinnjoseph77@gmail.com>
2025-02-28 17:35:04 -0800

    Did a little introduction to my portfolio by constructing the welcome hero section and then updated the projects section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Kelvin <br/>
+        Joseph a Software Engineer from Kenya
+      </SectionTitle>
+      <SectionText>
+      I specialize in front-end development, crafting visually stunning and highly interactive web applications. My expertise includes HTML, CSS, JavaScript, and TypeScript, along with modern frameworks and libraries like React, Next.js, Tailwind CSS, Bootstrap, and Material UI. I am passionate about building seamless, responsive, and high-performance user experiences. Always eager to learn and innovate, I aim to create impactful digital solutions that stand out.
+      </SectionText>
+      <Button onClick={() =>window.location="https://github.com/KelvinNJoseph"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
