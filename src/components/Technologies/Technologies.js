da371c2cc71d9a5fe19c65f18cd1c5b45780e13e
Kelvin Joseph <kelvinnjoseph77@gmail.com>
2025-02-28 17:50:05 -0800

    Updated my technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,52 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+    I've worked with a range of technologies in web development, focusing on front-end frameworks and tools.  
+    
+  </SectionText>
+    <List>
+      <ListItem>
+        <DiReact  size="3rem"/>
+        <ListContainer>
+          <ListTitle>
+            Front-End
+          </ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, Next.js, Javascript, 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase  size="3rem"/>
+        <ListContainer>
+          <ListTitle>
+          Web Technologies
+          </ListTitle>
+          <ListParagraph>
+          Proficient in <br />
+          HTML, CSS, Tailwind CSS, and Bootstrap
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem"/>
+        <ListContainer>
+          <ListTitle>
+          Version Control & Deployment
+          </ListTitle>
+          <ListParagraph>
+          Familiar with <br />
+          Git, GitHub,
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
